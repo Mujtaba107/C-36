d6e8ab0e61af0527660063badf660ea969a2b0a2
Mujtaba107 <62144950+Mujtaba107@users.noreply.github.com>
2020-07-17 19:54:07 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,21 @@
+class Form {
+constructor(){
+
+}
+display(){
+var title = createElement('h2','Car Racing Game')
+var input = createInput('name')
+var button = createButton('play')
+}
+
+
+
+}
+
+
+
+
+
+
+
+
